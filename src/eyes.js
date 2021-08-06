db93e796759f8073cf75437d9c68db6a426b5262
Jenna Ransom <jenna.ransom@gmail.com>
2021-08-06 15:45:57 -0600

    completes js to move eyeballs with mouse movement

--- src/eyes.js
+++ src/eyes.js
@@ -3,8 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
   const irisRight = document.querySelector('div.iris-right')
 
   const moveEye = function(tag, mouseX, mouseY) {
-    tag.style.left = mouseX + 'px'
-    tag.style.top = mouseY + 'px'
+    // center of eye 
+    const eyeMidX = tag.getBoundingClientRect().left
+    const eyeMidY = tag.getBoundingClientRect().top
+
+    // eye & mouse diff
+    const diffX = mouseX - eyeMidX
+    const diffY = mouseY - eyeMidY
+
+    // Pythagorean theorem
+    const diff = Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2))
+    
+    //capped radius
+    const radius = Math.min(3, diff)
+
+    // tan
+    const angle = Math.atan2(diffY, diffX)
+
+    // capped version 
+    const cappedX = radius * Math.cos(angle)
+    const cappedY = radius * Math.sin(angle)
+
+    const eyeTag = tag.querySelector('div')
+    eyeTag.style.left = cappedX + 'px'
+    eyeTag.style.top = cappedY + 'px'
   }
 
   document.addEventListener('mousemove', (e) => {
